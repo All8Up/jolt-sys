a2cd60f701cdb8045016889e7ce6fb7a61076454
Kelly Brock <all8up@gmail.com>
2022-03-16 19:45:30 -0400

    Initial submodule and build.

---
+++ jolt-sys/build.rs
@@ -0,0 +1,18 @@
+use std::path::{Path, PathBuf};
+
+#[cfg(feature = "bundled")]
+extern crate cmake;
+
+fn main() {
+    #[cfg(feature = "bundled")]
+    let path = compile_jolt(Path::new("./jolt/Build"));
+}
+
+#[cfg(feature = "bundled")]
+fn compile_jolt(build_path: &Path) -> PathBuf {
+    let mut config = cmake::Config::new(build_path);
+    config.profile("Release");
+    config.build_target("ALL_BUILD");
+
+    config.build()
+}
