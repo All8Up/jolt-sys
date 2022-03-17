775decae5d5212dfd7896c63f624deeb55a3f9bc
Kelly Brock <all8up@gmail.com>
2022-03-17 10:42:03 -0400

    Starting bindgen and wrapper lib.

--- jolt-sys/build.rs
+++ jolt-sys/build.rs
@@ -7,6 +7,9 @@ fn main() {
     // Compile Jolt.
     let compiled_lib_path = compile_jolt(Path::new("./jolt/Build"), &opt_level);
 
+    // Generate FFI.
+    generate_ffi(Path::new("./jolt/Jolt"));
+
     // Adjust the library source path based on the opt level.
     let lib_out_path = if opt_level == "0" {
         compiled_lib_path.join("build/Debug")
@@ -34,4 +37,30 @@ fn compile_jolt(build_path: &Path, opt_level: &str) -> PathBuf {
         config.profile("Release");
     }
     config.static_crt(true).build_target("Jolt").build()
+}
+
+fn generate_ffi(includes: &Path) {
+    // Generate the required FFI files from the Jolt headers.
+    let bindings = bindgen::Builder::default()
+        .clang_args(
+            [
+                "-x",
+                "c++",
+                "-std=c++17",
+                &format!("-I{}", includes.display()),
+            ]
+            .into_iter(),
+        )
+        .rustfmt_bindings(true)
+        .header(includes.join("Jolt.h").to_str().unwrap())
+        .allowlist_type("")
+        .allowlist_var("")
+        .allowlist_function("")
+        .generate()
+        .expect("Unable to generate ittnotify bindings.");
+
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("jolt_h.rs"))
+        .expect("Could not write bindings!");
 }
